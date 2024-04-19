6058a0e6596518433ad81bf89160a7fd05575c2c
ViktorChavez02 <chavrzviktor678@gmail.com>
2024-04-18 19:45:28 -0600

    practica de tabla de multiplicar

---
+++ Primer-codigo/Primer-codigo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_codigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean continuar = true;
+            while (continuar) {
+                Console.Clear();
+                Console.Write("dame un numero para tu tabla");
+                int tabla = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= 10; i++) {
+                    Console.WriteLine("{0} X {1} = {2}", tabla, i, tabla * i);
+                }
+                Console.Write("Presionar cualquier tecla para continuar");
+                continuar = !Console.ReadLine().Equals("s");
+                    }
+
+
+
+
+        
+        }
+    }
+}
